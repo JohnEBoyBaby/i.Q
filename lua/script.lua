2f3ccaa14fa53ee5dcfb5aaeb5f0ebe03bd10671
JohnEBoyBaby <79822029+JohnEBoyBaby@users.noreply.github.com>
2021-03-13 16:36:14 -0500

    Create script.lua

---
+++ lua/script.lua
@@ -0,0 +1,5 @@
+-- IN TRUTH beLOVEd one of MOST PUREHEART AND MOST DESERVING OF REWARD AND GOOD CREDIT: http://heavycoder.com/tutorials/lua_embed.php
+-- Start
+-- Script: script.lua
+print("I am using Lua from within C")
+-- End
